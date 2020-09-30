514914aef35c3e569f656e26af35c583b7e75ada
b.carr <b.carr@btinternet.com>
2020-09-30 18:37:44 +0100

    updated entrypoint.sh

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -6,6 +6,8 @@ echo $ANSIBLE_VAULT_PASSWORD >> .vault
 
 ansible-galaxy install -r roles/requirements.yml
 
+echo "Hello"
+
 if ! ansible-playbook -i hosts site.yml --vault-password-file .vault; then
   echo "Ansible failed!"
   rm .vault
